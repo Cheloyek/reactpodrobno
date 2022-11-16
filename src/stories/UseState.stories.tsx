dbf6db95c56cc579a84edc47d223e096c0f616de
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-11-16 23:18:15 +0300

    add useState with function

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'UseState'
+}
+
+function generateData() {
+    console.log('generateData')
+    return 7777777777
+}
+
+export const Example1 = () => {
+    console.log('example')
+    const [counter, setCounter] = useState(generateData)
+
+    const changer = (state: number) => state + 1
+
+    return <>
+        {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+}
