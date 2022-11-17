2777239d1bc8b0204cb2d54bdab8463fe1eb81ea
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-11-17 16:17:24 +0300

    add useTimeout, useInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -33,3 +33,43 @@ export const useEffectWithSideEffect = () => {
         {counter} {fake}
     </>
 }
+
+export const setTimeoutExample = () => {
+    const [fake, setFake] = useState(0)
+    const [counter, setCounter] = useState(0)
+    console.log('setTimeoutExample')
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('setTimeout')
+            document.title = counter.toString()
+        }, 1000)
+    }, [counter])
+
+    return <>
+        {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
+        <button onClick={() => setFake(fake + 1)}>fake</button>
+        <button onClick={() => setCounter(counter + 1)}>counter</button>
+        {counter} {fake}
+    </>
+}
+
+//timer
+export const setIntervalExample = () => {
+    const [counter, setCounter] = useState(0)
+    console.log('setIntervalExample')
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log('setInterval')
+            setCounter((state) => state + 1)
+        }, 1000)
+    }, [])
+
+    return <>
+        counter: {counter}
+    </>
+}
+
+//часы
+
