a6e3a842ce1ed8327c0374278524d7f20b921764
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-11-28 23:12:14 +0300

    add mode

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -4,12 +4,12 @@ import clock from './../../common/clock.png'
 import internal from "stream";
 
 type PropsType = {
-
+    mode?: 'digital' | 'analog'
 }
 
 const get2digitsString = (number: number) => number < 10 ? '0' + number : number
 
-export const Clock = () => {
+export const Clock = (props: PropsType) => {
     const [date, setDate] = useState(new Date())
 
     useEffect(() => {
@@ -32,13 +32,13 @@ export const Clock = () => {
             clearInterval(internalID) // stop tick when you go to another stories
         }
 
-    },[])
-    const deg = 360/60
+    }, [])
+    const deg = 360 / 60
     let hh = date.getHours() * 30
     let mm = date.getMinutes() * deg
     let ss = date.getSeconds() * deg
     const stylehr = {
-        transform: `rotateZ(${((hh) + (mm/12)) + 180}deg)`
+        transform: `rotateZ(${((hh) + (mm / 12)) + 180}deg)`
     }
     const stylemn = {
         transform: `rotateZ(${(mm) + 180}deg)`
@@ -47,25 +47,31 @@ export const Clock = () => {
         transform: `rotateZ(${(ss) + 180}deg)`
     }
 
-    return <div className={s.digitalClock}>
-        <span>{get2digitsString(date.getHours())}</span>
-        :
-        <span>{get2digitsString(date.getMinutes())}</span>
-        :
-        <span>{get2digitsString(date.getSeconds())}</span>
-        <div className={s.clockMain}>
-        <div className={s.clock} >
-            <img className={s.img} src={clock} alt=""/>
-            <div className={'hour'} >
-                <div className={s.hr} id={'hr'} style={stylehr}></div>
+    return <div>
+        {props.mode === 'digital' ? <>
+            <div className={s.digitalClock}>
+                <span>{get2digitsString(date.getHours())}</span>
+                :
+                <span>{get2digitsString(date.getMinutes())}</span>
+                :
+                <span>{get2digitsString(date.getSeconds())}</span>
             </div>
-            <div className={'min'}>
-                <div className={s.mn} id={'mn'} style={stylemn}></div>
+        </> : <>
+            <div className={s.clockMain}>
+                <div className={s.clock}>
+                    <img className={s.img} src={clock} alt=""/>
+                    <div className={'hour'}>
+                        <div className={s.hr} id={'hr'} style={stylehr}></div>
+                    </div>
+                    <div className={'min'}>
+                        <div className={s.mn} id={'mn'} style={stylemn}></div>
+                    </div>
+                    <div className={'sec'}>
+                        <div className={s.sc} id={'sc'} style={stylesc}></div>
+                    </div>
+                </div>
             </div>
-            <div className={'sec'}>
-                <div className={s.sc} id={'sc'} style={stylesc}></div>
-            </div>
-        </div>
-        </div>
+        </>
+        }
     </div>
 }
